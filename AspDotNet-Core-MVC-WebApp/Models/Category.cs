336a65a99984e89f691ca23b6d8233aa86657042
intikhab-h-bhat <intikhabh@gmail.com>
2024-07-26 09:30:58 +0530

    added pacakages entityFrameworek and models

---
+++ AspDotNet-Core-MVC-WebApp/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspDotNet_Core_MVC_WebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+
+    }
+}
