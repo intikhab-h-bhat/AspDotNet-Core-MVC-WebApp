cb0bd29e791769b744e2989b275aedb76373d1e1
intikhab-h-bhat <intikhabh@gmail.com>
2024-07-26 07:14:54 +0530

    Add project files.

---
+++ AspDotNet-Core-MVC-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspDotNet_Core_MVC_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
