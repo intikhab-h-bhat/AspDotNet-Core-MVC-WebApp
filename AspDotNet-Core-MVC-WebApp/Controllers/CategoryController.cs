8f3eb47de89b1becdc11defe205be42caa2d6de7
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2024-07-30 06:25:42 +0530

    added the controller for category (#3)

---
+++ AspDotNet-Core-MVC-WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNet_Core_MVC_WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
