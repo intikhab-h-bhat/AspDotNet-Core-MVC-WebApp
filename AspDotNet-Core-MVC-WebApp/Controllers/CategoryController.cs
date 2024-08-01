62ec2d30a4f4bd1b194261d7e18fd96f79e9e6b7
intikhab-h-bhat <intikhabh@gmail.com>
2024-08-01 22:22:21 +0530

    get the data from database to view

--- AspDotNet-Core-MVC-WebApp/Controllers/CategoryController.cs
+++ AspDotNet-Core-MVC-WebApp/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspDotNet_Core_MVC_WebApp.Data;
+using AspDotNet_Core_MVC_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspDotNet_Core_MVC_WebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCatogeryList=_dbContext.Catogries.ToList();
+
+            return View(objCatogeryList);
         }
     }
 }
