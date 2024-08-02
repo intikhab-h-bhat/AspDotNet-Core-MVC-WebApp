f95f14c1b1e7e29861abcde7e9979adc88e25763
intikhab-h-bhat <intikhabh@gmail.com>
2024-08-02 07:37:13 +0530

    changres in index file and added the style

--- AspDotNet-Core-MVC-WebApp/Controllers/CategoryController.cs
+++ AspDotNet-Core-MVC-WebApp/Controllers/CategoryController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspDotNet_Core_MVC_WebApp.Data;
+using AspDotNet_Core_MVC_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspDotNet_Core_MVC_WebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public CategoryController(ApplicationDbContext dbcontext)
+        {
+            _dbcontext=dbcontext;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _dbcontext.Catogries.ToList();
+
+
+            return View(objCategoryList);
         }
     }
 }
